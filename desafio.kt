f41a43a537bbc3d012610108f615f2fdb8dc5266
Kênia Crisoni <109874837+keniacrisoni@users.noreply.github.com>
2023-10-01 19:07:49 -0300

    Alterações de melhorias no  desafio de projeto

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,53 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+class Usuario(val nome: String, val nivel: Nivel)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+
+    fun matricular(vararg usuarios: Usuario) {
+        inscritos.addAll(usuarios)
+    }
+
+    fun usuariosInscritos() {
+        for (u in inscritos) {
+            var nome = u.nome
+            var nivel = u.nivel
+            println("$nome $nivel")
+        }
+    }
+
+    fun imprimirFormacao() {
+        println("Nome da Formação: $nome")
+        println("Conteúdos:")
+        for (conteudo in conteudos) {
+            println(" - ${conteudo.nome}, Duração: ${conteudo.duracao} minutos")
+        }
+        println("Usuários Inscritos:")
+        usuariosInscritos()
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario01 = Usuario("Maria", Nivel.BASICO)
+    val usuario02 = Usuario("Jose", Nivel.INTERMEDIARIO)
+    val usuario03 = Usuario("Ana", Nivel.AVANCADO)
+
+    val conteudo01 = ConteudoEducacional("Java para iniciantes")
+    val conteudo02 = ConteudoEducacional("Banco de dados relacional")
+    val conteudo03 = ConteudoEducacional("Desafio de projetos")
+
+    val conteudos: MutableList<ConteudoEducacional> = mutableListOf(conteudo01, conteudo02, conteudo03)
+
+    val formacao = Formacao("Desenvolvedor", conteudos)
+
+    formacao.matricular(usuario01, usuario02, usuario03)
+
+    // Print the formation details
+    formacao.imprimirFormacao()
 }
